80ae6644de7dcd5caa2fc3ad68de2f64aa6fbdfc
Almurat98 <almurat908@gmail.com>
2023-06-08 12:46:48 -0500

    practice

--- src/main/java/Practice/Assignment.java
+++ src/main/java/Practice/Assignment.java
@@ -1,19 +1,12 @@
 package Practice;
 
 
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class Assignment {
     public static void main(String[] args) {
-        //Question-1 Animal Feast
-        //All the animals are having a feast! Each animal is bringing one dish. There is just one rule: the
-        //dish must start and end with the same letters as the animal's name. For example, the great
-        //blue heron is bringing garlic naan and the chickadee is bringing chocolate cake.
-        //Write a method that takes the animal's name and dish as arguments and returns true or
-        //false to indicate whether the beast is allowed to bring the dish to the feast.
-        //Assume that beast and dish are always lowercase strings, and that each has at least two
-        //letters. Beast and dish may contain hyphens and spaces, but these will not appear at the
-        //beginning or end of the string. They will not contain numerals.
+
 
         String animal = "cat";
         String dish = "caramel shit";
@@ -21,7 +14,15 @@ public class Assignment {
 
     }
 
-
+    //Question-1 Animal Feast
+    //All the animals are having a feast! Each animal is bringing one dish. There is just one rule: the
+    //dish must start and end with the same letters as the animal's name. For example, the great
+    //blue heron is bringing garlic naan and the chickadee is bringing chocolate cake.
+    //Write a method that takes the animal's name and dish as arguments and returns true or
+    //false to indicate whether the beast is allowed to bring the dish to the feast.
+    //Assume that beast and dish are always lowercase strings, and that each has at least two
+    //letters. Beast and dish may contain hyphens and spaces, but these will not appear at the
+    //beginning or end of the string. They will not contain numerals.
     public static boolean checkIfDishMatchName(String name,String dish){
         name = name.replace("-", "").replace(" ", "");
         dish = dish.replace("-", "").replace(" ", "");
@@ -37,11 +38,28 @@ public class Assignment {
     }
 
 
+//Given a string, write a method to find the first non-repeating vowel (a, e, i, o, u) in it and
+//return its index. If it doesn't exist, return -1. Assume that all the characters of the String is
+//lowercase.
+//( Do not use String class functions other than charAt() and length()
+
+
+
+        public static int findFirstUniqueVowel(String str){
+
+        char[] vowels ={'a','e','i','o','u'};
+
+        for (int i =0;i<str.length();i++){
+
+        }
 
 
 
 
 
+
+
+        }
 
 
 
