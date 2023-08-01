46012fc004b44fcdb7dc89f070990793731fbcfe
Almurat98 <almurat908@gmail.com>
2023-08-01 14:56:58 -0500

    test

--- src/main/java/Practice/DesignBrowserHistory.java
+++ src/main/java/Practice/DesignBrowserHistory.java
@@ -5,44 +5,35 @@ import java.util.Deque;
 import java.util.Stack;
 
 public class DesignBrowserHistory {
-    String currentPage;
-    String home;
-    Deque <String>browsHistory,forwardStep;
-    public DesignBrowserHistory(String homepage) {
-        browsHistory.add(homepage);
-     currentPage= homepage;
-        browsHistory = new ArrayDeque();
-     forwardStep= new ArrayDeque();
 
+
+   Stack <String>browsHistory,forwardStep;
+    public DesignBrowserHistory(String homepage) {
+        browsHistory = new Stack<>();
+     forwardStep= new Stack<>();
+     browsHistory.add(homepage);
     }
 
     public void visit(String url) {
-
-        browsHistory.add(url);
-        currentPage= url;
-        forwardStep= new ArrayDeque();
+      while (!forwardStep.isEmpty()){
+          forwardStep.pop();
+      }
+      browsHistory.push(url);
     }
 
     public String back(int steps) {
-
-        while(!browsHistory.isEmpty()&&steps>0){
-            if(steps>browsHistory.size()||currentPage.equals(home)){
-                browsHistory =new ArrayDeque<>();
-
-            }
-            forwardStep.push(browsHistory.removeLast());
-            currentPage=browsHistory.peekLast();
-            steps--;
+        while(browsHistory.size()>1&&steps-->0) {
+            forwardStep.push(browsHistory.peek());
+            browsHistory.pop();
         }
-        return currentPage;
+        return browsHistory.peek();
     }
 
     public String forward(int steps) {
-        while(!forwardStep.isEmpty()&&steps>0){
-            browsHistory.add(currentPage);
-            currentPage=forwardStep.pop();
-            steps--;
+        while(!forwardStep.isEmpty()&&steps-->0){
+            browsHistory.add(forwardStep.peek());
+            forwardStep.pop();
         }
-        return currentPage;
+        return browsHistory.peek();
     }
 }
