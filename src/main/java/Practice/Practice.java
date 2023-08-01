866ea5b039af84c99040a465cc5cbdf23ce441f7
Almurat98 <almurat908@gmail.com>
2023-07-31 22:16:56 -0500

    test

--- src/main/java/Practice/Practice.java
+++ src/main/java/Practice/Practice.java
@@ -7,6 +7,24 @@ public class Practice {
 
     public static void main(String[] args) {
 
+        DesignBrowserHistory bh = new DesignBrowserHistory("leetcode.com");
+
+        bh.visit("google.com");
+     //   bh.visit("linkedin.com");
+        //System.out.println("Forward 1 step: "+bh.forward(1));
+        System.out.println("Back 7 step: "+ bh.back(7));
+        System.out.println("Back 7 step: "+ bh.back(7));
+//
+
+//        bh.visit("apple.com");
+//        bh.visit("oroe.com");
+//        bh.visit("lego.com");
+////        System.out.println("Forward 2 steps: "+ bh.forward(2));
+////        System.out.println("Back 2 steps: "+bh.back(2));
+//        System.out.println("Back 7 steps: "+bh.back(7));
+//        bh.visit("cup.com");
+//        System.out.println("Back 6 steps: "+bh.back(6));
+//        System.out.println("Forward 9 steps: "+ bh.forward(9));
 
     }
 }
